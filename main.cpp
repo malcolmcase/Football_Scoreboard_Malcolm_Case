4fa63be939bdfaa4b5a5126f4d83c1684e2c9736
Malcolm Case <102632901+malcolmcase@users.noreply.github.com>
2022-04-17 04:14:33 +0000

    Finished program

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 //Built April 13, 2022
 
 #include <iostream>
+#include <iomanip>
 #include <string> 
 #include "Scoreboard.h"
 #include "Input_Validation_Extended.h"
@@ -54,7 +55,7 @@ class Scoreboard
   public: 
     Scoreboard()
     {
-      half = 0; 
+      half = 1; 
       time = 900;
       down = 1;
       togo = 10;
@@ -73,38 +74,70 @@ class Scoreboard
     Team getVisitor() const { return visitor; }
     void showScoreboard()
     {
-      string color = ""; 
-      string reset = "\x1b[0m";
-      string teamColor = "\x1b[31;1m";
-      color = "\x1b[32;4m"; //green 
-      string score = "\x1b[36;1m"; //score color
-      cout << color << "FOOTBALL SCOREBOARD" << reset << "\n"; 
-      cout << home.getName() << "\t\t" << (time/60) << ":" << (time%60) <<  "\t\t" << visitor.getName() << "\t\t" << endl; 
-      cout << "\t" << home.getScore() << "\t\t" << half << "\t\t" << visitor.getScore() << endl; 
-      cout << "\t" << down << "down" << "\t\t\t\t\t\t" << togo << "to go";
-      cout << "Timeouts: " << home.getTimeoutCount() << "\t\t\t\t\t\t\t\t" << visitor.getTimeoutCount();
-      cout << "-----------------------------------";
-      cout << home.getCity() << "\t\t" << visitor.getCity() << "\t\t" << endl;
-      cout << home.getCoach() << "\t\t" << visitor.getCoach() << "\n";
-      for(int i = 0; i < 47; i++) { cout << "*"; } cout << endl;
+      string green = "\x1b[32;4m"; //green underlined
+      string blue = "\x1b[34m"; //blue bold
+      string red = "\x1b[31m"; //red bold
+      string reset = "\x1b[0m"; //reset color
+      cout << green << "FOOTBALL SCOREBOARD" << reset << "\n\n"; 
+      cout << blue << setw(20) << home.getName() << reset << setw(9) << (time/60) << ":";
+      if ( time%60 == 0)
+      {
+        cout << (time%60) << "0";
+      }
+      else
+      {
+        cout << (time%60);
+      }
+      cout << red << setw(28) << visitor.getName() << reset << endl; 
+      cout << blue << setw(20) << home.getScore() << reset << red << setw(40) << visitor.getScore() << reset << endl; 
+      cout << setw(31) << "Half " << half << endl;
+      cout << setw(19) << "Down " << down << setw(38) << "To Go " << togo << endl;
+      cout << setw(19) << "Timeouts: " << blue << home.getTimeoutCount() << reset << red << setw(40) << visitor.getTimeoutCount() << reset << endl;
+      cout << "------------------------------------------------------------\n";
+      cout << blue << setw(20) << home.getCity() << reset << red << setw(40) << visitor.getCity() << reset << endl;
+      cout << blue << setw(20) << home.getCoach() << reset << red << setw(40) << visitor.getCoach() << reset << endl << endl;
        
        //proces to show the home team status
-       cout << "Home> \t"; 
+       cout << setw(27) << "Home Team = \t"; 
        if(home.getHomeStatus() == true)
        {
-         cout << "Team 1: " << home.getName() << "*"; 
+         cout << blue << setw(5) << "Team 1: " << home.getName() << reset; 
        }
        else if(visitor.getHomeStatus() == true)
        {
-         cout << "Team 2: " << visitor.getName() << "*"; 
+         cout << red << setw(5) << "Team 2: " << visitor.getName() << reset; 
        }
         else
        {
          cout << "Error: "; 
        }
-       
-       cout  << endl; 
+
+      cout << endl;
+      cout << "------------------------------------------------------------\n";
+      cout << endl; 
     }
+};
+
+void submenu1()
+{ 
+  cout << "1 = Update Team 1 Name\n";
+  cout << "2 = Update Team 1 Coach Name\n";
+  cout << "3 = Update Team 1 City\n";
+  cout << "4 = Update Team 1 Score\n";
+  cout << "5 = Update Team 1 Timeout Count\n";
+  cout << "6 = Return to Main Menu...\n";
+  cout << ">";
+};
+
+void submenu2()
+{ 
+  cout << "1 = Update Team 2 Name\n";
+  cout << "2 = Update Team 2 Coach Name\n";
+  cout << "3 = Update Team 2 City\n";
+  cout << "4 = Update Team 2 Score\n";
+  cout << "5 = Update Team 2 Timeout Count\n";
+  cout << "6 = Return to Main Menu...\n";
+  cout << ">";
 };
 
 int main()
@@ -127,134 +160,181 @@ int main()
   //Set the home team 
   tOne.setHomeStatus(true); //tOne is the Home Team now   
 
-  //add some initial data to s
   s.setHome(tOne); 
   s.setVisitor(tTwo); 
-
+  
   do 
   {
-    system("clear"); //clear the screen of previous content 
-    s.showScoreboard(); //show the current scoreboard data
-    //menu choices 
-    cout << "A = Update Team 1\n" ; 
-    cout << "B = Update Team 2\n" ; 
-    cout << "C = Update Home Status\n" ; 
-    cout << "D = Update Time\n" ; 
-    cout << "E = Update Half\n";
-    cout << "F = Update Down\n";
-    cout << "G = Update Yards Togo\n";
-    cout << "X = Exit\n";
+    system("clear");
+    s.showScoreboard();
+    cout << "1 = Update Team 1\n" ; 
+    cout << "2 = Update Team 2\n" ; 
+    cout << "3 = Update Home Status\n" ; 
+    cout << "4 = Update Time\n" ; 
+    cout << "5 = Update Half\n";
+    cout << "6 = Update Down\n";
+    cout << "7 = Update Yards Togo\n";
+    cout << "0 = Exit\n";
     cout << ">"; 
     cin >> userChoice; 
 
-    if(userChoice == "A" || userChoice == "a")
+    if(userChoice == "1")
     {
       do
       {
         system("clear");
-        
-      }
-      //Dr_T challenge Accept a new name for s's home team
-      cout << "****Update Home Team Score module*** " << endl; 
-      cout << "\nPlease enter a new name for the home team: ";
-      cin >> newName; 
-      //change that home team's default name
-      tOne.setName(newName); //set tOne's data to the new desired name
+        s.showScoreboard();
+        submenu1();
+        cin >> userChoice;
+        if(userChoice == "1")
+        {
+          cout << "\nEnter new name for team 1: ";
+          cin >> newName;
+          tOne.setName(newName);
+        }
+        else if(userChoice == "2")
+        {
+          cout << "\nEnter new coach name for team 1: ";
+          cin >> newCoach;
+          tOne.setCoachName(newCoach);
+        }
+        else if(userChoice == "3")
+        {
+          cout << "\nEnter new city name for team 1: ";
+          cin >> newCity;
+          tOne.setCity(newCity);
+        }
+        else if(userChoice == "4")
+        {
+          cout << "\nEnter new score for team 1: ";
+          newScore = validateInt(newScore);
+          tOne.setScore(newScore);
+        }
+        else if(userChoice == "5")
+        {
+          cout << "\nEnter new timeout count for team 1: ";
+          newTimeoutCount = validateInt(newTimeoutCount);
+          tOne.setTimeoutCount(newTimeoutCount);
+        }
+        else if(userChoice == "6")
+        {
+          cout << "\nReturning to main menu...\n";
+        }
+        else
+        {
+          cout << "\nInvalid input. Try Again.\n";
+        }
+        s.setHome(tOne); //refresh the data in s to the new updates...
+        s.setVisitor(tTwo); //refresh the data
+      }while (userChoice != "6");
     }
-    else if(userChoice == "B" || userChoice == "b")
+    else if(userChoice == "2")
     {
-      cout << "\nUpdate Home Score Module****" << endl; 
-      cout << "\nPlease enter a new score for the home team: "; 
-      cin >> newScore; 
-      tOne.setScore(newScore);  //set the new score for tOne        
+      do
+      {
+        system("clear");
+        s.showScoreboard();
+        submenu2();
+        cin >> userChoice;
+        if(userChoice == "1")
+        {
+          cout << "\nEnter new name for team 2: ";
+          cin >> newName;
+          tTwo.setName(newName);
+        }
+        else if(userChoice == "2")
+        {
+          cout << "\nEnter new coach name for team 2: ";
+          cin >> newCoach;
+          tTwo.setCoachName(newCoach);
+        }
+        else if(userChoice == "3")
+        {
+          cout << "\nEnter new city name for team 2: ";
+          cin >> newCity;
+          tTwo.setCity(newCity);
+        }
+        else if(userChoice == "4")
+        {
+          cout << "\nEnter new score for team 2: ";
+          newScore = validateInt(newScore);
+          tTwo.setScore(newScore);
+        }
+        else if(userChoice == "5")
+        {
+          cout << "\nEnter new timeout count for team 2: ";
+          newTimeoutCount = validateInt(newTimeoutCount);
+          tTwo.setTimeoutCount(newTimeoutCount);
+        }
+        else if(userChoice == "6")
+        {
+          cout << "\nReturning to main menu...\n";
+        }
+        else
+        {
+          cout << "\nInvalid input. Try Again.\n";
+        }
+        s.setHome(tOne); //refresh the data in s to the new updates...
+        s.setVisitor(tTwo); //refresh the data
+      }while (userChoice != "6");
     }
-    else if(userChoice == "C" || userChoice == "c")
+    else if(userChoice == "3")
     {
-      cout << "\nUpdate Home Status Module****" << endl; 
-      cout << "\nWho is the home team: 1 = T1, 2 = T2, 3 = T3: "; 
+      cout << "\nEnter the home team.\n1 = Team 1, 2 = Team 2: "; 
       homeTeamQuestion = validateInt(homeTeamQuestion); 
      // cin >> homeTeamQuestion; 
       if(homeTeamQuestion == 1)
       {
         tOne.setHomeStatus(true); 
         tTwo.setHomeStatus(false); 
-        tThree.setHomeStatus(false);
       }
       else if(homeTeamQuestion == 2)
       {
         tOne.setHomeStatus(false); 
         tTwo.setHomeStatus(true);
-        tThree.setHomeStatus(false);
-      }
-      else if(homeTeamQuestion == 3)
-      {
-        tOne.setHomeStatus(false); 
-        tTwo.setHomeStatus(false);
-        tThree.setHomeStatus(true);
       }
       else
       {
         cout << "\nInvalid Input!" << endl;
-        sleep(2); 
       }
     }
-    else if(userChoice == "D" || userChoice == "d")
+    else if(userChoice == "4") //update time
     {
-        cout << "\nUpdate Visitor Coach Module****" << endl; 
-        cout << "\nPlease enter the visitor coach Name: "; 
-        cin >> newCoachName; 
-        tTwo.setCoachName(newCoachName); 
+      cout << "\nEnter the time left (in seconds): "; 
+      newTime = validateInt(newTime);
+      s.setTime(newTime); 
     }
-    else if(userChoice == "E" || userChoice == "e")
+    else if(userChoice == "5") //update half
     {
-      cout << "****Update Team 3 name module*** " << endl; 
-      cout << "\nPlease enter a new name for Team 3: ";
-      cin >> newName; 
-      tThree.setName(newName);
+      cout << "\nEnter the current half: ";
+      newHalf = validateInt(newHalf);
+      s.setHalf(newHalf);
     }
-    else if(userChoice == "F" || userChoice == "f")
+    else if(userChoice == "6") //update down
     {
-      cout << "****Update Team 3 city module\n";
-      cout << "\nPlease enter a city name for Team 3: ";
-      cin >> newCity;
-      tThree.setCity(newCity);
+      cout << "\nEnter the current down: ";
+      newDown = validateInt(newDown);
+      s.setDown(newDown);
     }
-    else if(userChoice == "G" || userChoice == "g")
+    else if(userChoice == "7") //yards togo
     {
-        cout << "\nUpdate Team 3 Coach Module****" << endl; 
-        cout << "\nPlease enter the Team 3 coach Name: "; 
-        cin >> newCoachName; 
-        tThree.setCoachName(newCoachName); 
+      cout << "\nEnter the current yards togo: "; 
+      newTogo = validateInt(newTogo);
+      s.setTogo(newTogo); 
     }
-    else if(userChoice == "H" || userChoice == "h")
-    {
-      cout << "\nUpdate Team 3 Score Module****" << endl; 
-      cout << "\nPlease enter a new score for Team 3: "; 
-      cin >> newScore; 
-      tThree.setScore(newScore);      
-    }
-    else if(userChoice == "I" || userChoice == "i")
-    {
-      cout << "\nUpdate Team 3 Foul Count Module****\n";
-      cout << "\nPlease enter a new foul count for Team 3\n";
-      cin >> newFoul;
-      tThree.setFoul(newFoul);
-    }
-    else if(userChoice == "X" || userChoice == "x")
+    else if(userChoice == "0")
     {
       cout << "Exit chosen." << endl; 
     }
-    else //invalid input: default clause
+    else //invalid input
     {
       cout << "\nInvalid input." << endl; 
-      sleep(2); //wait two seconds, go to the next process 
     }
 
     s.setHome(tOne); //refresh the data in s to the new updates...
     s.setVisitor(tTwo); //refresh the data
-    s.setThree(tThree);
 
-}while(userChoice != "X" && userChoice != "x");
+}while(userChoice != "0");
   
   return 0; 
 }
---
+++ main.d
@@ -0,0 +1,1 @@
+main.o main.d : main.cpp Scoreboard.h Input_Validation_Extended.h
